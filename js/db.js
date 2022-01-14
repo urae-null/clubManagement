a888758e93e0f9bb5b3791984c0cbcc272a9e953
ChungSangHyun <alrnd01@naver.com>
2022-01-14 21:22:19 +0900

    prototype

--- js/db.js
+++ js/db.js
@@ -1,9 +1,12 @@
+var data;
+var url;
+var clubNumber = 1;
+
 function loadClubList(){
-    let url = 'https://script.google.com/macros/s/AKfycbz48SfrCNoyzAC2Y4cassb96sI35bHVK6wXZ12gB4x9yr6JPzBeerH7Tj_DONO6js4ziQ/exec'
+    url = 'https://script.google.com/macros/s/AKfycbz48SfrCNoyzAC2Y4cassb96sI35bHVK6wXZ12gB4x9yr6JPzBeerH7Tj_DONO6js4ziQ/exec'
     fetch(url)
         .then(response => response.json())
         .then(json => {
-            console.log(json)
             data = json.list
             var clubExplain = document.getElementById('clubExplain')
             var firstList = document.getElementById('firstList')
@@ -17,6 +20,7 @@ function loadClubList(){
                 }
                 clubExplain.innerHTML += clubContainer(data[i])
             }
+            console.log(data)
         }).catch(() => {
             console.log('error')
     });
@@ -26,25 +30,25 @@ function clubContainer(data) {
     var container = `
     <div id="${data.name}" style="display:flex;">
     <div class="clubPoster">
-        <img style="width: 80%; margin: auto;">
+        <img id="posterC${data.name}" style="width: 80%; margin: auto;" src="${data.poster}">
     </div>
-    <div style="height: 360px; width: 50%;">
-        <div style="height: 87.5px;">
-            <img style="width: 31px; height: 31px; text-align: left;">
-            <p class="clubName">${data.name}</p>
+    <div style="height: 540px; width: 50%;">
+        <div style="height: calc(87.5px * 3 / 2);">
+            <img id="logoC${data.name}" style="width: calc(31px * 3 / 2); height: calc(31px * 3 / 2); text-align: left;" src="${data.logo}">
+            <p class="clubName" id="nameC${data.name}">${data.name}</p>
         </div>
-        <div class="clubType">제${data.group}동아리</div>
-        <div class="clubSubject">${data.subject}</div>
+        <div class="clubType" id="groupC${data.name}">제${data.group}동아리</div>
+        <div class="clubSubject" id="subjectC${data.name}">${data.subject}</div>
         <div class="clubIntroTitle">소개</div>
-        <div class="clubIntro">
+        <div class="clubIntro" id="introC${data.name}">
             ${data.intro}
         </div>
-        <footer style="margin-top: auto; margin-bottom: 8.5px;">
-            <div style="text-align: center; margin: 14px; display: flex; align-items: center; justify-content: center;">
-                <div class="clubInterview">면접<br>${data.interview}</div>
-                <div class="clubMyself">자소서<br>${data.myself}</div>
+        <footer style="margin-top: auto; margin-bottom: calc(8.5px * 3 / 2);">
+            <div style="text-align: center; margin: calc(14px * 3 / 2); display: flex; align-items: center; justify-content: center;">
+                <div class="clubInterview">면접<br><div id="interviewC${data.name}">${data.interview}</div></div>
+                <div class="clubMyself">자소서<br><div id="myselfC${data.name}">${data.myself}</div></div>
             </div>
-            <div class="clubInterviewDate">면접 일시 - ${data.date}</div>
+            <div class="clubInterviewDate">면접 일시- <div id="dateC${data.name}"> ${data.date}</div></div>
             </div>
         </footer>
     </div>
@@ -54,18 +58,46 @@ function clubContainer(data) {
 
 function moveY(cnt) {
     var clubExplain = document.getElementById("clubExplain");
-    clubExplain.style.transform = `translateY(-${cnt*360}px)`
+    clubExplain.style.transform = `translateY(-${cnt*540}px)`;
+    insertSign(cnt);
 }
 
-function sign(){
+function insertSign(cnt) {
     var appliForm = document.getElementById("application");
-    var schoolNumber = appliForm.schoolNumber.value;
-    var name = appliForm.name.value;
-    var firstClub1 = appliForm.firstClub1.value;
-    var firstClub2 = appliForm.firstClub2.value;
-    var secondClub1 = appliForm.secondClub1.value;
-    var secondClub2 = appliForm.secondClub2.value;
-    console.log(appliForm,schoolNumber,firstClub1, M.toast({html: '학번이 올바르지 않아요.',inDuration: 200, outDuration:200}));
+    clubNumber = cnt;
+    appliForm.name.value = data[cnt].name;
+    appliForm.group.value = data[cnt].group;
+    appliForm.subject.value = data[cnt].subject;
+    appliForm.intro.value = data[cnt].intro;
+    appliForm.interview.value = data[cnt].interview;
+    appliForm.date.value = data[cnt].date;
+    appliForm.myself.value = data[cnt].myself;
+    appliForm.poster.value = data[cnt].poster;
+    appliForm.logo.value = data[cnt].logo;
+    appliForm.link.value = data[cnt].link;
+}
+
+document.getElementById('application').addEventListener('change', function(event){
+    var elem = event.target;
+    console.log(event);
+    console.log(elem.tagName);
+    console.log(elem.type);
+    console.log(elem.value);  
+    if (elem.name != 'group' || elem.name != 'link' || elem.name != 'name') {
+        if(elem.name == 'poster' || elem.name =='logo') {
+            document.getElementById(elem.name+'C'+data[clubNumber].name).src = elem.value + ".jpg";
+        } else {
+            document.getElementById(elem.name+'C'+data[clubNumber].name).innerHTML = elem.value;
+        }
+    }
+});
+
+async function sign(){
+    var appliForm = document.getElementById("application");
+    appliForm.action = 'https://script.google.com/macros/s/AKfycbzyGNCbUKuxvzDoR7oP3L3bxc51zJTCqqmf7aud-SEfHAAhu5bsStunQ-88-YNm804PmA/exec';
+    await appliForm.submit();
+    M.toast({html: '수정되었습니다.',inDuration: 200, outDuration:200});
+    appliForm.preventDefault();
     return
     
 }
